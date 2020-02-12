4d0975a743c69aaa5ae499e25bfd15b7dab861e0
MrMokka Sartaren <sartaren@hotmail.com>
2020-02-12 11:27:34 +0100

    Question edit now easy to add/remove lines and alternatives

--- DevTest/Assets/Scripts/Admin/Admin_AddLine.cs
+++ DevTest/Assets/Scripts/Admin/Admin_AddLine.cs
@@ -4,4 +4,25 @@ using UnityEngine;
 
 public class Admin_AddLine : MonoBehaviour {
 
+	public GameObject Line;
+	public Transform SettingsButtonHolder;
+	public List<GameObject> LineList = new List<GameObject>();
+	
+
+	public void AddLine() {
+		GameObject line = Instantiate(Line, transform, false);
+		LineList.Add(line);
+		SettingsButtonHolder.SetAsLastSibling();
+	}
+
+	public void RemoveLine() {
+		if(LineList.Count > 0) {
+			GameObject toDelete = LineList[LineList.Count - 1];
+			LineList.Remove(toDelete);
+			DestroyImmediate(toDelete);
+
+		}
+	}
+
+
 }
