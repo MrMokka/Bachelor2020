9bb6850224c758fd782f109dda6b8c21b0888217
MrMokka Sartaren <sartaren@hotmail.com>
2020-02-17 13:27:44 +0100

    Panelcontroller v1 Admin

--- DevTest/Assets/Scripts/Admin/Admin_PanelController.cs
+++ DevTest/Assets/Scripts/Admin/Admin_PanelController.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Admin_PanelController : MonoBehaviour {
 
 
-	
+	public GameObject ActivePanel;
 
-
-
-
-
+	public void SetActivePanel(GameObject newPanel) {
+		if(ActivePanel == null) {
+			Debug.LogError("No active panel!");
+			return;
+		}
+		ActivePanel.SetActive(false);
+		ActivePanel = newPanel;
+		ActivePanel.SetActive(true);
+	}
 
 
 }
