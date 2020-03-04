0049b964379564851fa52fe3788ec28866b6180f
MrMokka Sartaren <sartaren@hotmail.com>
2020-03-04 11:17:00 +0100

    next question now takes time to activate to not trigger too early

---
+++ DevTest/Assets/Scripts/SpaceCollector/SC_NextQuestionButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SC_NextQuestionButton : MonoBehaviour {
+
+	public float TriggerTime;
+	public Image ProgressDisplay;
+	public GameLoader GameLoader;
+
+	private float Timer;
+	private bool IsInside;
+	private SC_ShipController ShipController;
+
+
+	void Update() {
+		if(IsInside) {
+			Timer += Time.deltaTime;
+			if(Timer >= TriggerTime) {
+				Timer = 0;
+				ShipController.Respawn();
+				GameLoader.NextQuestion();
+			}
+		} else {
+			if(Timer >= 0) {
+				Timer -= Time.deltaTime;
+			}
+		}
+		ProgressDisplay.fillAmount = Timer / TriggerTime;
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(!other.CompareTag("Player"))
+			return;
+		IsInside = true;
+		if(ShipController == null)
+			ShipController = other.GetComponent<SC_ShipController>();
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if(!other.CompareTag("Player"))
+			return;
+		IsInside = false;
+	}
+
+
+
+}
